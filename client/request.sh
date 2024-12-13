3a1263730f146aa2fe3278445ae1dfa92fe41107
kmin1231 <kmin91143@gmail.com>
2024-12-14 00:05:12 +0900

    style: 'sleep' added for delay in script

--- client/request.sh
+++ client/request.sh
@@ -5,11 +5,13 @@ BASE_URL="http://server:3000"
 
 echo "(1) Testing GET /students"
 curl -X GET "$BASE_URL/students"
+sleep 3
 echo -e "\n"
 
 
 echo "(2) Testing GET /students/:id"
 curl -X GET "$BASE_URL/students/1"
+sleep 3
 echo -e "\n"
 
 
@@ -20,14 +22,17 @@ curl -X POST "$BASE_URL/students" -d '{"name": "Peter", "age": 24, "score": 95}'
 echo -e "\n"
 
 curl -X GET "$BASE_URL/students"
+sleep 3
 echo -e "\n"
 
 
 echo "(4) Testing DELETE /students/:id"
 curl -X DELETE "$BASE_URL/students/2"
+
 echo -e "\n"
 
 curl -X GET "$BASE_URL/students"
+sleep 3
 echo -e "\n"
 
 
@@ -41,6 +46,7 @@ echo -e "\n"
 
 echo -e "after 'PUT' request"
 curl -X GET "$BASE_URL/students/1"
+sleep 3
 echo -e "\n"
 
 
@@ -50,6 +56,7 @@ curl -X GET "$BASE_URL/students/3"
 echo -e "\n"
 
 curl -X PATCH "$BASE_URL/students/3" -d '{"score": 91}'
+sleep 3
 echo -e "\n"
 
 
@@ -57,10 +64,12 @@ echo "(7) Testing POST /students/:id/grade"
 curl -X POST "$BASE_URL/students/1/grade"
 curl -X POST "$BASE_URL/students/3/grade"
 echo "grade updated for the score modified by 'PUT' & 'PATCH' request"
+sleep 3
 echo -e "\n"
 
 
 echo "(8) Testing GET /students/search"
 echo "search students with grade 'A'"
 curl -X GET "$BASE_URL/students/search?grade=A"
+sleep 3
 echo -e "\n"
